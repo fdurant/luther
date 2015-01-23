2a2ac8df14b2037979416d20d67edb751d1115c8
fdurant <frederik.durant@pandora.be>
2015-01-23 17:24:50 -0500

    Added parse methods for Title, Genre, Actors, Directors, Writers, Producers, Composers, DomesticTotalGross, ProductionBudget, RuntimeInMinutes and ReleaseDate

--- MoviePage.py
+++ MoviePage.py
@@ -2,7 +2,10 @@ from SomePage import SomePage
 import urllib2
 import os.path
 from bs4 import BeautifulSoup
-from myutils import myAssert
+from myutils import myAssert, removeTags, reduceWhiteSpace
+import re
+import sys
+import dateutil.parser
 
 class MoviePage(SomePage):
     ''' Represents a movie page from Box Office Mojo
@@ -28,13 +31,122 @@ class MoviePage(SomePage):
         return self.soup
  
     def __getMovieTitle__(self):
-        return "whatever"
+        try:
+            result = self.soup.find('font', {'face':'Verdana', 'size':'6'}).find('b').text
+        except:
+            result = ''
+        return result
+
+    def __getGenre__(self):
+        result=''
+        try:
+            pattern = re.compile("Genre:\s+<b>(.+?)<\/b>")
+            result = pattern.search(str(self.soup)).group(1)
+        except AttributeError:
+            pass
+        return result.lower()
+
+    def __getCompositeTableItem__(self, qualifier):
+        result = ''
+        try:
+            pattern = re.compile("%s:.*?<td>(.+?)<\/td>" % qualifier)
+            tmp = pattern.search(str(self.soup)).group(1)
+            tmpList = tmp.split('<br>')
+            result = [reduceWhiteSpace(removeTags(a)).replace('*','') for a in tmpList]
+            return "|".join(result)
+        except AttributeError:
+            pass
+        return result
+
+    def __getActors__(self):
+        result = ''
+        try:
+            pattern = re.compile("Actors:.*?<td>(.+?)<\/td>")
+            tmp = pattern.search(str(self.soup)).group(1)
+            tmpList = tmp.split('<br>')
+            result = [reduceWhiteSpace(removeTags(a)).replace('*','') for a in tmpList]
+            return "|".join(result)
+        except AttributeError:
+            pass
+        return result
+
+    def __getSingleTableItem__(self, qualifier, lowerCase=False):
+        result=''
+        try:
+            pattern = re.compile("%s:.*?<td>(.+?)<\/td>" % qualifier)
+            tmp = pattern.search(str(self.soup)).group(1)
+            result = reduceWhiteSpace(removeTags(tmp)).replace('*','')
+        except AttributeError:
+            pass
+        if (lowerCase):
+            return result.lower()
+        else:
+            return result
+
+    def __getDomesticTotalGross__(self):
+        result=''
+        try:
+            pattern = re.compile("Domestic Total Gross:\s+<b>\$([\d,]+?)<\/b>")
+            result = pattern.search(str(self.soup)).group(1)
+        except AttributeError:
+            pass
+        return result.replace(',','')
+
+    def __getProductionBudget__(self):
+        result=''
+        try:
+            pattern = re.compile("Production Budget:\s+<b>\$?([\d,N\/A]+?)<\/b>")
+            result = pattern.search(str(self.soup)).group(1)
+        except AttributeError:
+            pass
+        return result.replace(',','')
+
+    def __getRuntimeInMinutes__(self):
+        result=''
+        try:
+            pattern = re.compile("Runtime:\s+<b>([^<]*?)<\/b>")
+            tmp = pattern.search(str(self.soup)).group(1)
+            hours = re.search('(\d+)\s*ho?u?rs\.?',tmp)
+            if hours is None:
+                hh = 0
+            else:
+                hh=hours.group(1)
+            mins = re.search('(\d+)\s*min\.',tmp)
+            if mins is None:
+                mm = 0
+            else:
+                mm=mins.group(1)
+#            print >> sys.stderr, "found %s hrs and %s mins" % (hh, mm)
+            totalMins = int(mm) + (int(hh) * 60)
+            result = str(totalMins)
+        except AttributeError:
+            pass
+        return result
+
+    def __getReleaseDate__(self):
+        result=''
+        try:
+            pattern = re.compile("Release Date:\s+<b><nobr>(.+?)</nobr><\/b>")
+            result = pattern.search(str(self.soup)).group(1)
+        except AttributeError:
+            pass
+        return str(dateutil.parser.parse(result))
 
     def getCsvRow(self):
         ''' Returns a row of comma separated data '''
         self.__makeSoup__()
         result = []
         result.append(self.__getMovieTitle__())
+        result.append(self.__getGenre__())
+        result.append(self.__getActors__())
+        result.append(self.__getCompositeTableItem__('Directors?'))
+        result.append(self.__getCompositeTableItem__('Writers?'))  
+        result.append(self.__getCompositeTableItem__('Producers?'))  
+        result.append(self.__getCompositeTableItem__('Composers?'))  
+        result.append(self.__getDomesticTotalGross__())
+        result.append(self.__getProductionBudget__())
+        result.append(self.__getRuntimeInMinutes__())
+        result.append(self.__getReleaseDate__())
         return result
 
 if __name__ == "__main__":
@@ -61,4 +173,15 @@ if __name__ == "__main__":
 
     # Beautiful soup
     csvRow = mp2.getCsvRow()
-    myAssert(csvRow[0],'whatever','movie title')
+    myAssert(csvRow[0],'The Blues Brothers','Title')
+    myAssert(csvRow[1],'comedy','Genre')
+    myAssert(csvRow[2],'John Belushi|Dan Aykroyd|Carrie Fisher','Actors')
+    myAssert(csvRow[3],'John Landis','Directors')
+    myAssert(csvRow[4],'John Landis','Writers')
+    myAssert(csvRow[5],'Robert K. Weiss','Producers')
+    myAssert(csvRow[6],'Elmer Bernstein','Composers')
+    myAssert(csvRow[7],'57229890','DomesticTotalGross')
+    myAssert(csvRow[8],'N/A','ProductionBudget')
+    myAssert(csvRow[9],'150','RuntimeInMinutes')
+    myAssert(csvRow[10],'1980-06-20 00:00:00','Release Date')
+#    print >> sys.stderr, "cvsRow is ", csvRow
--- myutils.py
+++ myutils.py
@@ -1,2 +1,18 @@
-def myAssert(expected,actual,fieldname):
+import re
+
+def myAssert(actual,expected,fieldname):
     assert (actual == expected), "Incorrect %s: expected '%s', got '%s'" % (fieldname, expected, actual)
+
+def removeTags(string):
+    result = re.sub("<\/?[^>]+>",'',string)
+    return result
+
+def reduceWhiteSpace(string):
+    result = re.sub("\s+",' ',string)
+    return result
+
+if __name__ == "__main__":
+
+    myAssert(removeTags('<a href="http://some.website.com/">Some Website</a>'),"Some Website",'tag removal')
+    myAssert(removeTags('<b>Important!</b>'),"Important!",'tag removal')
+    myAssert(removeTags('<br/>'),"",'tag removal')
